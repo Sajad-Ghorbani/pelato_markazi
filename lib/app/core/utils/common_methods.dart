cf17843955bf97965504f29925a14516711b4bc4
sajad ghorbani <sajad.2012.gh@gmail.com>
2023-10-06 15:10:42 +0330

    refactor: create method for show toast

--- lib/app/core/utils/common_methods.dart
+++ lib/app/core/utils/common_methods.dart
@@ -1,4 +1,6 @@
-import 'package:intl/intl.dart';
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart' as intl;
+import 'package:toastification/toastification.dart';
 
 class CommonMethods {
   CommonMethods._();
@@ -6,19 +8,37 @@ class CommonMethods {
   static List<int> setDatesList(List<DateTime> dates) {
     List<int> dateIndices = [];
     for (int i = 0; i < dates.length; i++) {
-      if (DateFormat.yMd().format(dates[0]) !=
-          DateFormat.yMd().format(dates[i])) {
+      if (intl.DateFormat.yMd().format(dates[0]) !=
+          intl.DateFormat.yMd().format(dates[i])) {
         if (dateIndices.isEmpty) {
           dateIndices.add(i);
         } //
         else if (dateIndices.isNotEmpty &&
-            DateFormat.yMd().format(dates[i]) !=
-                DateFormat.yMd().format(dates[i - 1])) {
+            intl.DateFormat.yMd().format(dates[i]) !=
+                intl.DateFormat.yMd().format(dates[i - 1])) {
           dateIndices.add(i);
         } //
       }
     }
     dateIndices.insert(0, 0);
     return dateIndices;
+  }
+
+  static showToast(BuildContext context,String text){
+    toastification.show(
+      context: context,
+      type: ToastificationType.error,
+      style: ToastificationStyle.flat,
+      title: 'خطا',
+      description: text,
+      alignment: Alignment.topCenter,
+      autoCloseDuration: const Duration(seconds: 3),
+      borderRadius: BorderRadius.circular(12.0),
+      boxShadow: highModeShadow,
+      direction: TextDirection.rtl,
+      closeOnClick: false,
+      dragToClose: true,
+      pauseOnHover: false,
+    );
   }
 }
