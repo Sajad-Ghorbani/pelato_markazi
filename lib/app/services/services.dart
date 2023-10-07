8763ac9144416d6c499f9d5805bcb2618c290542
sajad ghorbani <sajad.2012.gh@gmail.com>
2023-10-07 16:28:05 +0330

    refactor: add salon and order usecases

--- lib/app/services/services.dart
+++ lib/app/services/services.dart
@@ -10,6 +10,14 @@ import 'package:pelato_markazi/data/repositories/user_repository_impl.dart';
 import 'package:pelato_markazi/domain/repositories/order_repository.dart';
 import 'package:pelato_markazi/domain/repositories/salon_repository.dart';
 import 'package:pelato_markazi/domain/repositories/user_repository.dart';
+import 'package:pelato_markazi/domain/use_cases/order/create_order_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/order/get_all_order_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/order/get_single_order_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/order/update_order_days_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/order/update_order_status_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/salon/get_all_salon_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/salon/get_coupon_use_case.dart';
+import 'package:pelato_markazi/domain/use_cases/salon/get_salon_use_case.dart';
 import 'package:pelato_markazi/domain/use_cases/user/complete_profile_use_case.dart';
 import 'package:pelato_markazi/domain/use_cases/user/confirm_code_use_case.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -33,6 +41,18 @@ class Services extends GetxService {
     ///user use cases
     Get.put<CompleteProfileUseCase>(CompleteProfileUseCase(Get.find()));
     Get.put<ConfirmCodeUSeCase>(ConfirmCodeUSeCase(Get.find()));
+
+    ///salon use cases
+    Get.put<GetSalonUseCase>(GetSalonUseCase(Get.find()));
+    Get.put<GetCouponUseCase>(GetCouponUseCase(Get.find()));
+    Get.put<GetAllSalonsUseCase>(GetAllSalonsUseCase(Get.find()));
+
+    ///order use cases
+    Get.put<CreateOrderUseCase>(CreateOrderUseCase(Get.find()));
+    Get.put<GetAllOrderUseCase>(GetAllOrderUseCase(Get.find()));
+    Get.put<GetSingleOrderUseCase>(GetSingleOrderUseCase(Get.find()));
+    Get.put<UpdateOrderDaysUseCase>(UpdateOrderDaysUseCase(Get.find()));
+    Get.put<UpdateOrderStatusUseCase>(UpdateOrderStatusUseCase(Get.find()));
 
     return this;
   }
