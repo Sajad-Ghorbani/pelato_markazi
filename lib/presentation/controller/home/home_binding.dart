c14bddafbdf936edf309a4c3f81f6a72cb269858
sajad ghorbani <sajad.2012.gh@gmail.com>
2023-10-07 16:53:43 +0330

    feat: get all salon from use case

--- lib/presentation/controller/home/home_binding.dart
+++ lib/presentation/controller/home/home_binding.dart
@@ -4,6 +4,6 @@ import 'package:pelato_markazi/presentation/controller/home/home_controller.dart
 class HomeBinding implements Bindings{
   @override
   void dependencies() {
-    Get.put<HomeController>(HomeController(),permanent: true);
+    Get.put<HomeController>(HomeController(Get.find()),permanent: true);
   }
 }
--- lib/presentation/controller/home/home_controller.dart
+++ lib/presentation/controller/home/home_controller.dart
@@ -1,18 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:pelato_markazi/app/core/resources/data_state.dart';
-import 'package:pelato_markazi/data/models/order_meta_data.dart';
-import 'package:pelato_markazi/data/models/order_model.dart';
-import 'package:pelato_markazi/data/models/salon_model.dart';
-import 'package:pelato_markazi/data/models/user_model.dart';
-import 'package:pelato_markazi/data/providers/order_api.dart';
-import 'package:pelato_markazi/data/providers/salon_api.dart';
-import 'package:pelato_markazi/data/providers/user_api.dart';
+import 'package:pelato_markazi/app/core/utils/common_methods.dart';
+import 'package:pelato_markazi/app/services/services.dart';
+import 'package:pelato_markazi/domain/entities/salon_entity.dart';
+import 'package:pelato_markazi/domain/use_cases/salon/get_all_salon_use_case.dart';
 import 'package:pelato_markazi/presentation/pages/home/about_us_screen.dart';
 import 'package:pelato_markazi/presentation/pages/home/home_screen.dart';
 import 'package:pelato_markazi/presentation/pages/order/orders_screen.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class HomeController extends GetxController {
+  final GetAllSalonsUseCase _allSalonsUseCase;
+
+  HomeController(this._allSalonsUseCase);
+
   int currentIndex = 0;
   List<Widget> widgets = const [
     HomeScreen(),
@@ -20,72 +21,35 @@ class HomeController extends GetxController {
     AboutUsScreen(),
   ];
 
-  List<SalonModel> salonList = salons;
+  List<SalonEntity> salonList = [];
+
+  @override
+  void onReady() {
+    super.onReady();
+    getSalons(Get.context!);
+
+  }
 
   void changePage(int index) {
     currentIndex = index;
     update();
   }
 
-  void getData() async {
-    String token =
-        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY1MTEyYWE1OGFiZDVlODg2NjkyNDk3ZSIsImlhdCI6MTY5NjM1NzI1NCwiZXhwIjoxNzI3OTE0ODU0fQ.e6dVSSpcgJ7QcJHMfqWJDlWAdoemSBAqdMBvVNhwO9I';
-    // await UserApi()
-    //     .confirmCode(phone: '09351679934', confirmCode: '2087')
-    //     .then((confirm) async {
-    //   print(confirm);
-    // });
-    // if (confirm != null) {
-    //   token = confirm;
-    // }
-    // if (token != '-1') {
-    print('--------------------------------------');
-    String? complete = await UserApi().completeProfile(
-      user: User(
-        name: 'sajad',
-        familyName: 'ghorbani',
-        email: 'sajad.2012.gh@gmail.com',
-        instagramPageAddress: 'sajad_ghorbani10',
-        groupName: ' ',
-      ),
-      token: token,
-    );
-    print(complete);
-    List<SalonModel>? allSalon = await SalonApi().getAllSalon(token: token);
-    print(allSalon);
-    SalonModel? salon =
-        await SalonApi().getSalon(id: '651132488abd5e88669249ba', token: token);
-    print(salon);
-    DataState<bool> crOrder = await OrderApi().createOrder(
-      salonId: '651132488abd5e88669249ba',
-      token: token,
-      // couponCode: 'f2h6a8x6h9',
-      reserveDay: [
-        {
-          "day": '2023-10-05 12:00:00.000',
-          "hours": "9-10"
-        },
-        {
-          "day": '2023-10-05 12:00:00.000',
-          "hours": "10-11"
-        },
-        {
-          "day": '2023-10-05 12:00:00.000',
-          "hours": "11-12"
-        },
-        {
-          "day": '2023-10-05 12:00:00.000',
-          "hours": "12-13"
-        }
-      ],
-    );
-    print(crOrder.data ?? crOrder.error);
-    (OrderMetaData, List<OrderModel>)? allOrder =
-        await OrderApi().getAllOrder(token: token);
-    print(allOrder);
-    OrderModel? order =
-        await OrderApi().getOrder(id: allOrder!.$2[0].id!, token: token);
-    print(order);
+  void getSalons(BuildContext context) async {
+    SharedPreferences pref = Get.find<Services>().pref;
+    String token = pref.getString('token') ?? '-1';
+    if (token != '-1') {
+      var response = await _allSalonsUseCase.execute(token: token);
+      if (response.data != null) {
+        salonList = response.data!;
+        update();
+      } //
+      else {
+        CommonMethods.showToast(
+          context,
+          'خطایی در دریافت اطلاعات به وجود آمده است. دوباره تلاش کن',
+        );
+      }
+    }
   }
-// }
 }
