b11a8b8a2650e8d1b67ba43974b744b9c2c70a9b
sajad ghorbani <sajad.2012.gh@gmail.com>
2023-10-08 00:09:44 +0330

    reactor: export data dependeces

---
+++ lib/data/data.dart
@@ -0,0 +1,6 @@
+export 'providers/order_api.dart';
+export 'providers/salon_api.dart';
+export 'providers/user_api.dart';
+export 'repositories/order_repository_impl.dart';
+export 'repositories/salon_repository_impl.dart';
+export 'repositories/user_repository_impl.dart';
